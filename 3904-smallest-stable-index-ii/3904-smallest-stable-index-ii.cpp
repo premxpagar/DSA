63a17c4da217ae455c637f945982dc9ac83b9218
Prem Pagar <pagarprem27@gmail.com>
2026-09-05 20:55:59 +0530

    Time: 4 ms (89.3%), Space: 202.7 MB (76.32%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> suf(n);
+        suf[n-1] = nums[n-1];
+
+        for (int i = n-2; i >= 0; --i)
+            suf[i] = min(nums[i], suf[i+1]);
+
+        int mx = 0;
+        for (int i = 0; i < n; ++i) {
+            mx = max(mx, nums[i]);
+            if (mx - suf[i] <= k) return i;
+        }
+        return -1;
+    }
+};
